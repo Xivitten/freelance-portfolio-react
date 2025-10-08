2e332a66f445730de232b14de9c77e8592b9be75
xivitten <sindormant58@yandex.ru>
2025-10-08 18:03:43 +0300

    ADD FULL FUNCTIONAL BEFORE LIGHT AND NIGHT THEME

---
+++ src/pages/Contacts.js
@@ -0,0 +1,27 @@
+const Contacts = () => {
+  return ( 
+    <main className="section">
+        <div className="container">
+                <h1 className="title-1">Contacts</h1>
+
+                <ul className="content-list">
+                    <li className="content-list__item">
+                        <h2 className="title-2">Location</h2>
+                        <p>Penza, Russia</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Telegram / WhatsApp</h2>
+                        <p><a href="tel:+79051234567">+7 (986) 731-34-00</a></p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Email</h2>
+                        <p><a href="mailto:webdev@protonmail.com">sindormant58@yandex.ru</a></p>
+                    </li>
+                </ul>
+
+        </div>
+    </main>
+  );
+}
+ 
+export default Contacts;
